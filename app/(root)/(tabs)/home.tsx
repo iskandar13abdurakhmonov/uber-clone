02e54d5e06e141c81c534d53a3a4acf4c812b106
Iskander <iskandar.13.aburahmonov@gmail.com>
2025-08-25 14:46:16 +0500

    flatlist and google places search

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,9 +1,13 @@
-import {ActivityIndicator, FlatList, Image, Text, TouchableOpacity, View} from "react-native";
+import {ActivityIndicator, FlatList, Image, Text, TouchableOpacity, View } from "react-native";
+import * as Location from 'expo-location';
 import {SafeAreaView} from "react-native-safe-area-context";
 import {useUser} from "@clerk/clerk-expo";
 import RideCard from "@/components/RideCard";
 import {icons, images} from "@/constants";
 import GoogleTextInput from "@/components/GoogleTextInput";
+import Map from "@/components/Map";
+import {useLocationStore} from "@/store";
+import {useEffect, useState} from "react";
 
 const recentRides = [
     {
@@ -106,8 +110,13 @@ const recentRides = [
 
 export default function Home() {
 
+    const { setUserLocation, setDestinationLocation } = useLocationStore()
+
+
     const { user } = useUser()
     const loading = false
+
+    const [hasPermissions, setHasPermissions] = useState(false)
 
     const handleSignOut = () => {
 
@@ -116,6 +125,31 @@ export default function Home() {
     const handleDestinationPress = () => {
 
     }
+
+    useEffect(() => {
+        const requestLocation = async () => {
+            let { status } = await Location.requestBackgroundPermissionsAsync()
+
+            if(status !== 'granted') {
+                setHasPermissions(false)
+                return
+            }
+
+            let location = await Location.getCurrentPositionAsync()
+
+            const address = await Location.reverseGeocodeAsync({
+                latitude: location.coords?.latitude!,
+                longitude: location.coords?.longitude!
+            })
+
+            setUserLocation({
+                latitude: location.coords.latitude,
+                longitude: location.coords.longitude,
+                address: `${address[0].name}, ${address[0].region}`
+            })
+        }
+        requestLocation()
+    }, [])
 
     return (
         <SafeAreaView className="bg-general-500">
@@ -156,6 +190,19 @@ export default function Home() {
                             containerStyle="bg-white shadow-md shadow-neutral-300"
                             handlePress={handleDestinationPress}
                         />
+
+                        <>
+                            <Text className="text-xl font-JakartaBold mt-5 mb-3">
+                                Your Current Location
+                            </Text>
+                            <View className="flex flex-row items-center bg-transparent h-[300px]">
+                                <Map />
+                            </View>
+                        </>
+
+                        <Text className="text-xl font-JakartaBold mt-5 mb-3">
+                            Recent Rides
+                        </Text>
                     </>
                 )}
             />
---
+++ components/Map.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import MapView, { PROVIDER_DEFAULT } from 'react-native-maps';
+import {Text} from "react-native";
+
+const Map = () => {
+    return (
+            <MapView
+                provider={PROVIDER_DEFAULT}
+                className="w-full h-full rounded-2xl"
+            >
+                <Text>Map</Text>
+            </MapView>
+    );
+};
+
+export default Map;
